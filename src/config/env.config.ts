01cd1edf4f11d6252cdd7cb1d01cf6193e2fdbcf
John <barretodiazjohnalexander@gmail.com>
2024-11-05 14:18:41 -0500

    Se cambio configuracion de la varialbe port para railway

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,6 @@
 export const EnvConfiguration = () => ({
     enviroment: process.env.NODE_ENV || "dev",
     mongodb: process.env.MONGODB || "mongodb://localhost:27017/nest-pokemon",
-    port: parseInt(process.env.PORT) || 3000,
+    port: parseInt(process.env.PORT_DEV) || 3000,
     defaultLimit: +process.env.DEFAULT_LIMIT || 5
 })
